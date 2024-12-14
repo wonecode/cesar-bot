0925eff387b68d13ef8d1ed11196ce03eef8170d
wonecode <lochlainn.gadault@gmail.com>
2024-12-14 17:14:02 +0100

    feat(monkey generator): improved adjective dataset

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-import { TextChannel, Client } from 'discord.js';
-import { createButtons } from '../components/monkey-generator/buttons';
-import { createEmbed } from '../components/monkey-generator/embed';
+import { Client } from 'discord.js';
 
 export const getMembersCount = (client: Client) => {
   const guildId = '1314380138305163356';
@@ -20,30 +18,5 @@ export const getMembersCount = (client: Client) => {
     });
   } else {
     console.log(`Le serveur avec l'ID ${guildId} n'a pas été trouvé.`);
-  }
-};
-
-export const sendMessageWithEmbedAndButtons = async (
-  client: Client,
-  channelId: string,
-) => {
-  const channel = client.channels.cache.get(channelId) as TextChannel;
-
-  if (!channel) {
-    console.error(`Channel avec l'ID ${channelId} introuvable.`);
-    return;
-  }
-
-  const buttons = createButtons();
-  const embed = createEmbed();
-
-  try {
-    await channel.send({
-      embeds: [embed],
-      components: [buttons],
-    });
-    console.log('Message avec embed et boutons envoyé avec succès.');
-  } catch (error) {
-    console.error('Erreur lors de l’envoi du message :', error);
   }
 };
